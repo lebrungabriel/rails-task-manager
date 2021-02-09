bb5b2e9c2241d05996167680e6248c50891c0294
Gabriel Lebrun <lebr1.gabriel@gmail.com>
2021-02-09 17:00:18 +0100

    task manager done

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_02_09_104939) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
